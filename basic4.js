6f2f34d27db2a32373d5e3fbe7358566cbc9a14c
samirvithlani <samir.vithlani83955@gmail.com>
2024-10-15 17:55:00 +0530

    const

---
+++ basic4.js
@@ -0,0 +1,12 @@
+//const -->block...
+
+let x =100;
+console.log("value of x = ",x)
+//var x =100; redeclaration error
+x =120; //no error /// reassign
+console.log("value of x = ",x)
+
+
+const pi = 3.14;
+console.log("value of pi = ",pi)
+//pi = 3.15; //error //reassign
